88c00c6dbaded1b29d75e944b1bdf4e1edd74850
leoquiroa <leo@gethitch.ai>
2022-03-25 16:29:00 -0600

    reorder folder

---
+++ Applications/Qixe/passing_yearbooks.py
@@ -0,0 +1,14 @@
+
+def findSignatureCounts(arr):
+    pass
+
+
+if __name__ == '__main__':
+    R = []
+    R.append([2, 1]) # [2, 2]
+    R.append([1, 2]) # [1, 1]
+    for r in R:
+        print(findSignatureCounts(r[0],r[1]),r)
+        
+
+    
